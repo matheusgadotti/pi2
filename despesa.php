64c82ef194df4cd8aadc7b38e138a3a8b3abec57
Matheus <matheusgmeriz@hotmail.com>
2022-10-17 21:09:25 -0300

    Cabecalho

--- despesa.php
+++ despesa.php
@@ -2,53 +2,9 @@
 
 include('protect.php');
 
+require_once('cabecalho.php');
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="style.css">
-    <title>SIGEFI - Despesa</title>
-</head>
-<body>
-<header>
-        <div class="cabeca">
-            <nav>
-                <section class="sessao1">
-                    <div class="blocoLado_a_Lado">
-                        <a href="home.php">Home </a>
-                    </div>
-
-                    <div class="blocoLado_a_Lado">
-                        <a href="consulta.php">Consulta </a>
-                    </div>
-
-                    <div class="blocoLado_a_Lado">
-                        <a href="receita.php">Receita </a>
-                    </div>
-
-                    <div class="blocoLado_a_Lado">
-                        <a href="">Despesa </a>
-                    </div>
-
-                    <div class="blocoLado_a_Lado">
-                        <a href="investimento.php">Investimento </a>
-                    </div>
-
-                    <div class="blocoLado_a_Lado">
-                        <a href="relatorio.php">Relatorio </a>
-                    </div>
-
-                    <div class="blocoLado_a_Lado">
-                        <a href="logout.php">Sair</a>
-                    </div>
-                </section>
-            </nav>
-        </div>
-    </header>
     <div>
     <div class="box">
     <form action="">
--- home.php
+++ home.php
@@ -2,58 +2,9 @@
 
 include('protect.php');
 
+require_once('cabecalho.php');
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="style.css">
-    <title>Spa</title>
-</head>
-<body>
-    <header>
-        <div class="cabeca">
-            <nav>
-                <section class="sessao1">
-                    <div class="blocoLado_a_Lado">
-                        <a href="">Home </a>
-                    </div>
-
-                    <div class="blocoLado_a_Lado">
-                        <a href="consulta.php">Consulta </a>
-                    </div>
-
-                    <div class="blocoLado_a_Lado">
-                        <a href="receita.php">Receita </a>
-                    </div>
-
-                    <div class="blocoLado_a_Lado">
-                        <a href="despesa.php">Despesa </a>
-                    </div>
-
-                    <div class="blocoLado_a_Lado">
-                        <a href="investimento.php">Investimento </a>
-                    </div>
-
-                    <div class="blocoLado_a_Lado">
-                        <a href="relatorio.php">Relatorio </a>
-                    </div>
-
-                    <div class="blocoLado_a_Lado">
-                        <a href="logout.php">Sair</a>
-                    </div>
-                    
-                </section>
-            </nav>
-        </div>
-    </header>
-
-<main id="root">
-
-</main>
-    
     <footer>
     <p>&#169; Todos os diretiros reservados - 2022 - Matheus Gadotti Meriz</p>
   </footer>
